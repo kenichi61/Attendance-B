b6ef7c3f89f8e75d048c9b46005e918886f3aa18
kenichi <k32118076@gmail.com>
2023-12-08 04:52:42 +0000

    Finish login-function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
+  
   def new
   end
-  
-  def create
+
+  def create 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
@@ -11,5 +12,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
 end
